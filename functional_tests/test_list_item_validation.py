9c6058c24941aae39951c8186854efc439d7fb28
brucedockeray-projects <bruce@djangodata.com>
2014-10-14 23:49:20 +0100

    cleared rogue character from form - amended related functional test

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -18,8 +18,8 @@ class ItemValidationTest(FunctionalTest):
         # The home page refreshes, and there is an error message saying
         # that list items cannot be blank
         error = self.browser.find_element_by_css_selector('.has-error')
-        self.assertEqual(error.text, "You can't have an empty list item\n2")
-        #NB  I had to add the \n2 onto the test to get the test assertion to pass !!
+        self.assertEqual(error.text, "You can't have an empty list item")
+        
 
         # She tries again with some text for the item, which now works
         self.get_item_input_box().send_keys('Buy milk\n')
@@ -31,8 +31,8 @@ class ItemValidationTest(FunctionalTest):
         # She receives a similar warning on the list page
         self.check_for_row_in_list_table('1: Buy milk')
         error = self.browser.find_element_by_css_selector('.has-error')
-        self.assertEqual(error.text, "You can't have an empty list item\n2")
-        #NB  I had to add the \n2 onto the test to get the test assertion to pass !!
+        self.assertEqual(error.text, "You can't have an empty list item")
+        
 
         # And she can correct it by filling some text in
         self.get_item_input_box().send_keys('Make tea\n')
